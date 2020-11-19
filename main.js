25866a393b721a2839260eddf9f19f50bb7e61f3
bitna kim <tallshe108@hanmail.net>
2020-11-19 17:41:46 +0700

    modify gif

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const guess = (event) => {
     resultHTML = `<img src="https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif"/ width=300 height=168>
     <h1 class="result-text">정답!</h1>`;
   } else if (computerNumber < userNumber) {
-    resultHTML = `<img src="https://lh3.googleusercontent.com/proxy/FdEVh72TH2834LorcUF4lo5K6nII1xI9WH4i9knvDZx6uGSNUiFL9ykppf1tkoK6SuAa3leSwwelzjpT7U1XPVPEvOJZTG5k1JykiFooP1jR"/ width=300 height=168>
+    resultHTML = `<img src="https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif"/ width=300 height=168>
       <h1 class="result-text">Down!</h1>`;
   } else if (computerNumber > userNumber) {
     resultHTML = `<img src="https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif"/ width=300 height=168>
