908f2773df66eaf6672eb4af4524d51e2349893a
Bitna Kim <tallshe108@hanmail.net>
2021-10-24 16:16:19 +0900

    add more feature(chances limit, number range limit, duplicate limit)

--- main.js
+++ main.js
@@ -1,28 +1,82 @@
-let computerNumber = Math.floor(Math.random() * 100 + 1);
+// 필요한 html elements 다 가져오기
+let computerNumber = 0;
+let playButton = document.getElementById("play-button");
+let resetButton = document.querySelector(".button-reset");
+let userInput = document.querySelector("#user-input");
+let resultAreaImg = document.querySelector(".main-img");
+let resultText = document.querySelector(".result-text");
+let chanceArea = document.getElementById("chance-area");
+let gameOver = false;
+let chances = 5; // 남은 기회
+let userValueList = []; // 유저가 입력한 숫자들 리스트
 
-const guess = (event) => {
-  event.preventDefault();
-  const userNumber = document.getElementById("guessNumber").value;
-  let resultHTML = ``;
-  if (computerNumber == userNumber) {
-    resultHTML = `<img src="https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif"/ width=300 height=168>
-    <h1 class="result-text">정답!</h1>`;
-  } else if (computerNumber < userNumber) {
-    resultHTML = `<img src="https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif"/ width=300 height=168>
-      <h1 class="result-text">Down!</h1>`;
-  } else if (computerNumber > userNumber) {
-    resultHTML = `<img src="https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif"/ width=300 height=168>
-      <h1 class="result-text">Up!</h1>`;
+chanceArea.innerHTML = `남은 기회:${chances}`;
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", focusInput);
+
+function pickRandomNumber() {
+  // 랜덤숫자 뽑기
+  computerNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+function play() {
+  // 숫자 추측하기
+  const userValue = userInput.value;
+  if (userValue < 1 || userValue > 100) {
+    resultText.textContent = "1부터 100 사이의 숫자를 입력 해주세요";
+
+    return;
   }
 
-  document.getElementById("resultArea").innerHTML = resultHTML;
-  document.getElementById("guessNumber").value = ``;
-};
+  if (userValueList.includes(userValue)) {
+    resultText.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
 
-const reset = () => {
-  computerNumber = Math.floor(Math.random() * 10 + 1);
-  document.getElementById(
-    "resultArea"
-  ).innerHTML = `<img src="https://media1.giphy.com/media/9DinPR8bzFsmf74j9W/giphy.gif"/ width=300>
-    <h1 class="result-text">죽기싫다면 맞춰라!</h1>`;
-};
+    return;
+  }
+
+  chances--;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList.push(userValue);
+  if (userValue < computerNumber) {
+    resultAreaImg.src =
+      "https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif";
+    resultText.textContent = "Up!";
+  } else if (userValue > computerNumber) {
+    resultAreaImg.src = "https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif";
+    resultText.textContent = "Down!";
+  } else {
+    resultAreaImg.src =
+      "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif";
+    resultText.textContent = "정답!";
+    gameOver = true;
+  }
+
+  if (chances == 0) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function focusInput() {
+  userInput.value = "";
+}
+
+function reset() {
+  //리셋
+  pickRandomNumber();
+  userInput.value = "";
+  resultAreaImg.src =
+    "https://media1.giphy.com/media/9DinPR8bzFsmf74j9W/giphy.gif";
+  resultText.textContent = "죽기 싫다면 맞춰라";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+}
+
+pickRandomNumber();
+console.log("정답", computerNumber);
